e3a4ddfbe737e2dceae54b2d894ccaff9df22e8b
Maciej Rymar <maciej.rymar@protonmail.com>
2020-12-06 14:56:51 +0100

    Basic project structure, top navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HistoryComponent } from './pages/history/history.component';
+import { UploadComponent } from './pages/upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'upload', component: UploadComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: '',   redirectTo: '/upload', pathMatch: 'full' },
+  { path: '**', component: UploadComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UploadComponent } from './pages/upload/upload.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { NavTopComponent } from './components/nav-top/nav-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    HistoryComponent,
+    NavTopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
